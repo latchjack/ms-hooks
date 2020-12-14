31420bafcc7981c683e277139e0f07f597ebde7b
Latch Jackn <latch.jack@gmail.com>
2020-12-14 11:42:46 +0000

    Multiple states added

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -1,57 +1,50 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-import Card from "../UI/Card";
-import "./IngredientForm.css";
+import Card from '../UI/Card';
+import './IngredientForm.css';
 
 const IngredientForm = React.memo((props) => {
-  const [inputState, setInputState] = useState({ title: "", amount: "" });
+	const [enteredTitle, setEnteredTitle] = useState('');
+	const [enteredAmount, setEnteredAmount] = useState('');
 
-  const submitHandler = (event) => {
-    event.preventDefault();
-    // ...
-  };
+	const submitHandler = (event) => {
+		event.preventDefault();
+		// ...
+	};
 
-  return (
-    <section className="ingredient-form">
-      <Card>
-        <form onSubmit={submitHandler}>
-          <div className="form-control">
-            <label htmlFor="title">Name</label>
-            <input
-              type="text"
-              id="title"
-              value={inputState.title}
-              onChange={(event) => {
-                const newTitle = event.target.value;
-                setInputState((prevInputState) => ({
-                  title: newTitle,
-                  amount: prevInputState.amount,
-                }));
-              }}
-            />
-          </div>
-          <div className="form-control">
-            <label htmlFor="amount">Amount</label>
-            <input
-              type="number"
-              id="amount"
-              value={inputState.amount}
-              onChange={(event) => {
-                const newAmount = event.target.value;
-                setInputState((prevInputState) => ({
-                  amount: newAmount,
-                  title: prevInputState.title,
-                }));
-              }}
-            />
-          </div>
-          <div className="ingredient-form__actions">
-            <button type="submit">Add Ingredient</button>
-          </div>
-        </form>
-      </Card>
-    </section>
-  );
+	return (
+		<section className="ingredient-form">
+			<Card>
+				<form onSubmit={submitHandler}>
+					<div className="form-control">
+						<label htmlFor="title">Name</label>
+						<input
+							type="text"
+							id="title"
+							value={enteredTitle}
+							onChange={(event) => {
+								setEnteredTitle(event.target.value);
+							}}
+						/>
+					</div>
+					<div className="form-control">
+						<label htmlFor="amount">Amount</label>
+						<input
+							type="number"
+							id="amount"
+							value={enteredAmount}
+							onChange={(event) => {
+								setEnteredAmount(event.target.value);
+							}}
+						/>
+					</div>
+					<div className="ingredient-form__actions">
+						<button type="submit">Add Ingredient</button>
+					</div>
+				</form>
+			</Card>
+		</section>
+	);
 });
 
 export default IngredientForm;
